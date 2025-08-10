9aef06967402e0b5dd00ac071604515e6c3c7ff0
Cool-fire <upendrareddy2511@gmail.com>
2025-08-10 11:45:39 +0100

    Add VpcStack

--- unitune-infra/bin/unitune-infra.ts
+++ unitune-infra/bin/unitune-infra.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { UnituneInfraStack } from '../lib/unitune-infra-stack';
+import { VpcStack } from '../lib/stacks/vpc-stack';
 
 const app = new cdk.App();
-new UnituneInfraStack(app, 'UnituneInfraStack', {
+new VpcStack(app, 'VpcStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
---
+++ unitune-infra/lib/stacks/vpc-stack.ts
@@ -0,0 +1,27 @@
+import { Construct } from "constructs/lib/construct";
+import * as cdk from "aws-cdk-lib";
+import { FlowLogDestination, FlowLogTrafficType, Vpc } from "aws-cdk-lib/aws-ec2";
+import { Tags } from "aws-cdk-lib";
+
+export class VpcStack extends cdk.Stack {
+    public readonly vpc: Vpc;
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        this.vpc = new Vpc(this, 'VpcStack', {
+            vpcName: 'UnituneVpc',
+            maxAzs: 99, // Maximum number of Availability Zones to use
+            natGateways: 2,
+            flowLogs: {
+                VpcFlowlogs: {
+                    trafficType: FlowLogTrafficType.ALL,
+                    destination: FlowLogDestination.toCloudWatchLogs(),
+                }
+            }
+        });
+
+        this.vpc.privateSubnets.forEach((subnet) => {
+            Tags.of(subnet).add('karpenter.sh/discovery', 'true');
+        });
+    }
+}
